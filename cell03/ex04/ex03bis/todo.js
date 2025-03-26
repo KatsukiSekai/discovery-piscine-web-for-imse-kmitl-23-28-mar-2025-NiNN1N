f4c96514050da4addc7bd7ae203fedcde63b8afb
NiNN1N <ninjapool4567@gmail.com>
2025-03-26 23:56:14 +0700

    All changes

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -2,38 +2,34 @@ $(document).ready(function() {
     loadTasks();
 
     $("#new-task").click(function() {
-        let task = prompt("Enter a new task:");
-        if (task) {
-            addTask(task);
-            saveTasks();
-        }
+      let task = prompt("Enter a new task:");
+      if (task) {
+        addTask(task);
+        saveTasks();
+      }
     });
 
     function addTask(task) {
-        let div = $("<div></div>").text(task).addClass("task");
-        div.click(function() {
-            if (confirm("Do you want to remove this task?")) {
-                $(this).remove();
-                saveTasks();
-            }
-        });
-        $("#ft_list").prepend(div);
+      let div = $("<div></div>").text(task).addClass("task");
+      div.click(function() {
+        if (confirm("Do you want to remove this task?")) {
+          $(this).remove();
+          saveTasks();
+        }
+      });
+      $("#ft_list").prepend(div);
     }
 
     function saveTasks() {
-        let tasks = [];
-        $("#ft_list div").each(function() {
-            tasks.push($(this).text());
-        });
-        document.cookie = "tasks=" + JSON.stringify(tasks) + "; path=/";
+      let tasks = [];
+      $("#ft_list div").each(function() {
+        tasks.push($(this).text());
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
 
     function loadTasks() {
-        let cookies = document.cookie.split("; ");
-        let taskCookie = cookies.find(row => row.startsWith("tasks="));
-        if (taskCookie) {
-            let tasks = JSON.parse(taskCookie.split("=")[1]);
-            tasks.forEach(task => addTask(task));
-        }
+      let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+      tasks.reverse().forEach(task => addTask(task));
     }
 });
